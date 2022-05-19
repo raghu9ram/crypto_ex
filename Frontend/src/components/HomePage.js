bccc2c119d33fe728ff798aa5b88f7d8e3fb74b3
raghu9ram <73301319+raghu9ram@users.noreply.github.com>
2022-05-19 11:17:43 +0100

    few tweaks

--- Frontend/src/components/HomePage.js
+++ Frontend/src/components/HomePage.js
@@ -122,7 +122,7 @@ const Homepage = () => {
                      </div>
                    </td>
                    <td className="px-6 py-4 whitespace-nowrap">
-                     <div className="text-sm text-gray-900">$ {stock.current_price}</div>
+                     <div className="text-sm text-gray-900">£ {stock.current_price}</div>
                    </td>
                    <td className="px-6 py-4 whitespace-nowrap">
                      <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">
