7ff182e7981664291d08a17ef2120178484e5894
raghu9ram <73301319+raghu9ram@users.noreply.github.com>
2022-05-19 20:24:42 +0100

    make it betters

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ var cors = require('cors')
 connectToMongo();
 const app = express()
 app.use(cors())
-const port = 5000
+const port = process.env.PORT || 5000;
 app.use(express.json());
 // Available Routes
 app.use('/api/auth', require('./routes/auth'))
